c5a4173e569e06d1f8173c1df6bee3c6ed90dc60
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-05 03:30:01 -0700

    init

---
+++ main_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# views.py
+
+# Add this cats list below the imports
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+  {'name': 'Tsuki', 'breed': 'tuxedo', 'description': 'gentle, loving, best cat around', 'age': 2},
+]
+
+
+# Create your views here.
+def home(request):
+    #include an .html file extension - unlike when rendering EJS templates 
+    return render(request, 'main_app/home.html')
+
+def about(request):
+    return render(request, 'main_app/about.html')
+
+def cats_index(request):
+    return render(request, 'main_app/cats/index.html', {'cats': cats})
