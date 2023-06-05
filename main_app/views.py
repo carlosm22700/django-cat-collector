a70eb2353733ffbe53469d1428b6e5d0074300cf
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-05 04:49:19 -0700

    WIP: quety cat model inside of views

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-
-# views.py
-
-# Add this cats list below the imports
-cats = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-  {'name': 'Tsuki', 'breed': 'tuxedo', 'description': 'furry little demon, but also gentle, loving, best cat around', 'age': 2},
-]
-
+from .models import Cat
 
 # Create your views here.
 def home(request):
@@ -19,4 +10,5 @@ def about(request):
     return render(request, 'main_app/about.html')
 
 def cats_index(request):
+    cats = Cat.objects.all()
     return render(request, 'main_app/cats/index.html', {'cats': cats})
