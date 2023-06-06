202468f270acdebc9e0d91f343e9979b9f55c875
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-06 02:00:05 -0700

    wip: adds unassoc feature

--- main_app/urls.py
+++ main_app/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path("toys/<int:pk>", views.ToyDetail.as_view(), name="toy_detail"),
     path("toys/<int:pk>/update", views.ToyUpdate.as_view(), name="toy_update"),
     path("toys/<int:pk>/delete", views.ToyDelete.as_view(), name="toy_delete"),
-    path('cats/<int:cat_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy')
+    path('cats/<int:cat_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+    path('cats/<int:cat_id>/unassoc_toy/<int:toy_id>/', views.unassoc_toy, name='unassoc_toy')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -40,6 +40,10 @@ def assoc_toy(request, cat_id, toy_id):
     Cat.objects.get(id=cat_id).toys.add(toy_id)
     return redirect('detail', cat_id=cat_id)
 
+def unassoc_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.remove(toy_id)
+    return redirect('detail', cat_id=cat_id)
+
 class CatCreate(CreateView):
     model = Cat
     fields = ['name', 'breed', 'description', 'age']
